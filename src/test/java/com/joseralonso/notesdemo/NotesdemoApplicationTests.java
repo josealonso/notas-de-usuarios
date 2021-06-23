ced796825a12a267ec69e37a6bf4b56e72bb694b
josealonso <jose.alonso.programmer@gmail.com>
2021-06-23 23:57:01 +0200

    Added dependencies for testing. Tested the application context loading.

--- src/test/java/com/joseralonso/notesdemo/NotesdemoApplicationTests.java
+++ src/test/java/com/joseralonso/notesdemo/NotesdemoApplicationTests.java
@@ -1,14 +1,24 @@
 package com.joseralonso.notesdemo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.joseralonso.notesdemo.restcontroller.NotesRestController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class NotesdemoApplicationTests {
 
+	@Autowired
+	NotesRestController notesRestController;
+	
 	@Test
 	void contextLoads() {
-		System.out.println("Hola, mundo");
+	    Assertions.assertThat(notesRestController).isNot(null);
 	}
 
 }
