17416c085a43b26b22e390ea0f8fcac4e72fae8e
josealonso <jose.alonso.programmer@gmail.com>
2021-06-23 18:16:34 +0200

    Added Spring Boot main class (it had been deleted)

--- src/main/java/com/joseralonso/notesdemo/NotesdemoApplication.java
+++ src/main/java/com/joseralonso/notesdemo/NotesdemoApplication.java
@@ -1,5 +1,9 @@
-import org.springframework.boot.SpringApplication;
+package com.joseralonso.notesdemo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NotesdemoApplication {
 
 	public static void main(String[] args) {
